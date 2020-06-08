aed7b359a5de6570dd704398ef407d43828373e8
Karl von Randow <karl@xk72.com>
2020-06-08 17:05:13 +1200

    punch 1.3.1

--- Formula/punch.rb
+++ Formula/punch.rb
@@ -1,8 +1,8 @@
 class Punch < Formula
   desc "A script to bootstrap, query and control AWS EC2 instances."
   homepage "https://github.com/karlvr/punch"
-  url "https://github.com/karlvr/punch/archive/1.3.tar.gz"
-  sha256 "fb73d7882cde99543fa93df915727fe89528ba702f06cde148a2aa77581f3ae1"
+  url "https://github.com/karlvr/punch/archive/1.3.1.tar.gz"
+  sha256 "a708d330c2bc0593573c0c52ad2a3ce627483be05c6ecbe07ac70edfeb6eabe8"
 
   def install
     inreplace "punch", /^PUNCH_LIB_DIR=.*/, "PUNCH_LIB_DIR=#{lib}"
