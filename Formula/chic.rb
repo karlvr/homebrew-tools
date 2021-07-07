ee732093c2bbdb9c3146f74670dd751e176b72b2
Karl von Randow <karl@xk72.com>
2021-07-07 14:25:26 +1200

    chic: 1.10.2

--- Formula/chic.rb
+++ Formula/chic.rb
@@ -1,8 +1,8 @@
 class Chic < Formula
   desc "Small utility to make AMIs"
   homepage "https://github.com/karlvr/chic"
-  url "https://github.com/karlvr/chic/archive/1.10.1.tar.gz"
-  sha256 "f8975d6704f31fd2eb6bdbd7f2a5c04a86a6d3bd04f987534ed95aacf74f6aad"
+  url "https://github.com/karlvr/chic/archive/1.10.2.tar.gz"
+  sha256 "98c50f12b1b0f4b6420ef183724f9c88028b7da560ae19d66d8ae5258c382ac6"
 
   def install
     inreplace "chic", /^CHIC_LIB_DIR=.*/, "CHIC_LIB_DIR=#{libexec}"
